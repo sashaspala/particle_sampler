b520d8c3cd56c96e7848d2919036a50c5f570d00
Sasha Spala <spala@usc.edu>
2016-04-05 21:09:12 -0700

    After 4/5 lab

--- lab10.py
+++ lab10.py
@@ -1,6 +1,8 @@
 import lab10_map
 import math
 import particle_filter
+import create2
+import odometry
 
 class Run:
     def __init__(self, factory):
@@ -16,36 +18,59 @@ class Run:
         # Add the IP-address of your computer here if you run on the robot
         self.virtual_create = factory.create_virtual_create()
         self.map = lab10_map.Map("lab10.map")
-        self.pf = particle_filter.ParticleFilter
+        self.pf = particle_filter.ParticleFilter()
+        self.odometry = odometry.Odometry()
 
     def run(self):
         # This is an example on how to visualize the pose of our estimated position
         # where our estimate is that the robot is at (x,y,z)=(0.5,0.5,0.1) with heading pi
-        self.virtual_create.set_pose((0.5, 0.5, 0.1), math.pi)
+        #self.virtual_create.set_pose((0.5, 0.5, 0.1), math.pi)
 
         # This is an example on how to show particles
         # the format is x,y,z,theta,x,y,z,theta,...
-        data = [0.5, 0.5, 0.1, math.pi/2, 1.5, 1, 0.1, 0]
-        self.virtual_create.set_point_cloud(data)
+       # data = [0.5, 0.5, 0.1, math.pi/2, 1.5, 1, 0.1, 0]
+        #self.virtual_create.set_point_cloud(data)
 
         # This is an example on how to estimate the distance to a wall for the given
         # map, assuming the robot is at (0, 0) and has heading math.pi
         print(self.map.closest_distance((0.5,0.5), math.pi))
 
+        self.create.start()
+        self.create.safe()
+        self.create.start_stream([create2.Sensor.LeftEncoderCounts, create2.Sensor.RightEncoderCounts])
+
+        #turning so we actually get a value!!
+        self.create.drive_direct(-100,100)
+        self.time.sleep(1.9)
+        self.create.drive_direct(0,0)
+
+        self.pf.senseOnce(self.create, self.sonar)
         # This is an example on how to detect that a button was pressed in V-REP
         while True:
             b = self.virtual_create.get_last_button()
+            state = self.create.update()
             if b == self.virtual_create.Button.MoveForward:
-                self.pf.movement(self, 0, self.create)
+                self.create.drive_direct(100,100)
+                self.time.sleep(1)
+                self.create.drive_direct(0,0)
                 print("Forward pressed!")
             elif b == self.virtual_create.Button.TurnLeft:
-                self.pf.movement(self, 1, self.create)
+                self.create.drive_direct(100,-100)
+                self.time.sleep(1.9)
+                self.create.drive_direct(0,0)
                 print("Turn Left pressed!")
             elif b == self.virtual_create.Button.TurnRight:
-                self.pf.movement(self, 2, self.create)
+                self.create.drive_direct(-100,100)
+                self.time.sleep(1.9)
+                self.create.drive_direct(0,0)
                 print("Turn Right pressed!")
             elif b == self.virtual_create.Button.Sense:
-                self.pf.sensing(self, self.create, self.sonar)
+                self.pf.sensing(self.create, self.sonar, self.virtual_create)
                 print("Sense pressed!")
 
+            self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
+            self.locationX = self.odometry.x
+            self.locationY = self.odometry.y
+            self.orientation = self.odometry.theta
+            self.pf.movement(self.locationX, self.locationY, self.orientation)
             self.time.sleep(0.01)
--- particle_filter.py
+++ particle_filter.py
@@ -1,8 +1,9 @@
 import numpy as np
-import scipy
-import create2
+from scipy import stats, misc
 import time
-import odometry
+import lab10_map
+import math
+import factory
 
 
 class ParticleFilter:
@@ -12,74 +13,109 @@ class ParticleFilter:
         self.locationX = 0
         self.locationY = 0
         self.orientation = 0
-        self.odometry = odometry.Odometry()
+
         self.distance = 0
 
-        self.probSensorGivenLoc = 0
+        #self.probSensorGivenLoc = 0
         self.numParticles = 100
-        self.probLoc = 1/self.numParticles
-        self.sumProbs = self.probLoc
+        self.probLoc = []
+        for index in range(0, self.numParticles - 1):
+            self.probLoc.append(math.log(1) - math.log(self.numParticles))
+
+        self.sumProbs = 1/self.numParticles
+
+        self.realMean = 0
+        self.realStd = 0
+        self.particleSense = []
+        for index in range(0, self.numParticles-1):
+            self.particleSense.append(0)
+
+        self.map = lab10_map.Map("lab10.map")
+        #self.virtual_create.set_pose((0.5, 0.5, 0.1), math.pi)
+
     #single particle first
 
-    def movement(self, value, create):
-        state = create.update()
-        if value == 0: #forward
-            create.drive_direct(100,100)
-            self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
-            self.locationX = self.odometry.x
-            self.locationY = self.odometry.y
-            self.orientation = self.odometry.theta
-            self.time.sleep(.5)
-        elif value == 1: #turn left
-            create.drive_direct(100,-100)
-            self.time.sleep(.1)
-        else: #turn right
-            create.drive_direct(-100,100)
-            self.time.sleep(.1)
+    def movement(self, x, y, theta):
+        self.locationX = x
+        self.locationY = y
+        self.orientation = theta
 
-    def sensing(self, create, sonar):
-        distanceArr = []
-        for x in range(0,self.numParticles):
-            distanceArr[x] = sonar.get_distance()
-        mean = np.average(distanceArr)
-        std = np.std(distanceArr)
-        self.probSensorGivenLoc = scipy.stats.norm(mean, std).pdf
+    def senseOnce(self, create, sonar):
+        self.realStd = .001
 
+        """ distanceArr = []
+        for x in range(0, self.numParticles-1):
+            distanceArr.append(sonar.get_distance())
 
-    def resample(self, probability, particles):
-        pass
+        print(distanceArr)
+        self.realMean = np.average(distanceArr)
+        self.realStd = np.std(distanceArr)
+        print("STD", self.realStd)
+        #self.probSensorGivenLoc = stats.norm.pdf(self.realMean, self.realStd)
+        """
 
-    def estimation(self):
+    def sensing(self, create, sonar, virtual_create):
+        sensedDistance = sonar.get_distance()
+        self.estimation(sensedDistance, virtual_create)
+
+    def estimation(self, sensedDistance, virtual_create):
         #step 1: create 100 randomly distributed particles
 
-        particleX = np.random.randint(0, 100, self.numParticles)
-        particleY = np.random.randint(0, 100, self.numParticles)
-        particleTheta = np.random.radint(0,180,self.numParticles)
+        particleX = np.random.uniform(0, 3, self.numParticles)
+        particleY = np.random.uniform(0, 3, self.numParticles)
+        particleTheta = np.random.uniform(0,2*math.pi, self.numParticles)
+
+
+        data = []
+        for index in range(0, self.numParticles-1):
+            data.append(particleX[index])
+            data.append(particleY[index])
+            data.append(0)
+            data.append(particleTheta[index])
+        virtual_create.set_pose((0.5, 0.5, 0.1), math.pi)
+        virtual_create.set_point_cloud(data)
 
         #getting sensor readings for each of the random robots
-        particleSense = []
+
         probabilities_toWeight = []
-        sumProbs = 0;
-        for reading in range(0, self.numParticles):
+        virtualProbSensor = []
+        for index in range(0, self.numParticles-1):
+            virtualProbSensor.append(0)
+
+        sum = []
+        for reading in range(0, self.numParticles-1):
             #sensor reading for each virtual robot
-            particleSense[reading] = self.map.closest_distance((particleX[reading], particleY[reading]), particleTheta[reading])
-            N = 1/sum(self.sumProbs)
+            self.particleSense[reading] = self.map.closest_distance((particleX[reading], particleY[reading]), particleTheta[reading])
+            #N = 1/self.sumProbs
 
-            #HERE: NEED TO GET WEIGHTS/PROBABILITIES OF THE NEW SAMPLES SOMEHOW??
+            #create random normal distribution and get probability
+            randDist = np.random.normal(self.realMean, self.realStd)
 
-            probability = (self.particleSense[reading] * self.probLoc) * N
-            probabilities_toWeight[reading] = probability
+            virtualProbSensor[reading] = (stats.norm.pdf(sensedDistance, self.particleSense[reading], np.std(randDist)))
+            #overall probability of this reading
+            sum.append(virtualProbSensor[reading] * self.probLoc[reading])
 
-        mean = np.average(particleSense)
-        std = np.std(particleSense)
+        totalSum = 0
+        for prob in range(0, self.numParticles-1):
+            totalSum = sum[prob] + totalSum
+
+        N = 1/totalSum
+        for reading in range(0, self.numParticles-1):
+            probability = (virtualProbSensor[reading] * (self.probLoc[reading])) * N
+            probabilities_toWeight.append(probability)
+
+
+        #DIVIDE BY ONE ERROR: probably coming from getting NaN or infinite value somewhere up here ^^^
 
         #now that we have P(virtual robot), kill off useless ones
-        resample(probability, particleSense)
-
-        #TO DO: compare probabilities of particle sensor values to probability defined above (line 65)
-
+        resampledRobots = np.random.choice(self.particleSense, self.numParticles, True, probabilities_toWeight)
+        copyOfResample = []
+        for index in range(0, self.numParticles-1):
+            copyOfResample.append(resampledRobots[index])
+        print("Resampled: ", resampledRobots)
+        self.particleSense = resampledRobots
         #updating probabilities
-        self.probLoc = probability
+        self.probLoc = probabilities_toWeight
         self.sumProbs += self.sumProbs
         #if particle is within 1 std deviation of mean value,
 
